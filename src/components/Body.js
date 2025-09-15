acd0a9a3dbcf8d30ae4512f96079bba7f38c6c44
ManishKumar <manishmmm1904@gmail.com>
2025-09-15 22:44:32 +0530

    "updated"

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { HashRouter as BrowserRouter,Routes,Route } from "react-router-dom"
 import Browse from "./Browse"
 import Login from "./Login"
 import Header from "./Header"
